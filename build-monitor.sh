809f9d858999fcd097ae58836c3886aa2e246adc
Andy Crockett <andy@crockett.io>
2018-08-08 20:35:38 -0500

    no more autorun after build

--- build-monitor.sh
+++ build-monitor.sh
@@ -2,4 +2,3 @@ cd /home/pi/app
 #gcc test.c -Wall -o test `pkg-config --cflags --libs glib-2.0` -lwiringPi -I/usr/include/postgresql -L/usr/lib/arm-linux/gnueabihf -lpq
 gcc monitor.c -Wall -o cwmonitor -lwiringPi -I/usr/include/postgresql -L/usr/lib/arm-linux/gnueabihf -lpq -lm -ldl
 cd -
-/home/pi/app/cwmonitor
